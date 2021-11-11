fb5eca8133abb57a500f8934b7c26ffb07f517ea
Ville Viklund <ville.viklund@geniem.com>
2021-11-11 15:51:58 +0200

    Add autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '11a53aa7a1449a17dd241ba924e9131af5fe1b9d',
+    'name' => 'devgeniem/wp-linkedevents',
+  ),
+  'versions' => 
+  array (
+    'devgeniem/wp-linkedevents' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '11a53aa7a1449a17dd241ba924e9131af5fe1b9d',
+    ),
+  ),
+);
